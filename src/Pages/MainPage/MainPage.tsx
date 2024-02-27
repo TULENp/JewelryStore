2ad07758f43d2d12caaa10dbe2bd39d50b5915d4
TULENp <zh_kononenko3@mail.ru>
2024-02-27 12:16:41 +0300

    Add pagination

--- src/Pages/MainPage/MainPage.tsx
+++ src/Pages/MainPage/MainPage.tsx
@@ -1,30 +1,49 @@
 import React, { useEffect, useState } from 'react';
 import { ItemCard } from '../../components/ItemCard';
+import styles from './MainPage.module.css';
 import { TItem } from '../../types/types';
 import { GetBrands, GetItems, GetFilteredIds, GetIds } from '../../service';
+import { Pagination } from '../../components/Pagination';
 
 //* Display search bar, filters, item cards list
 export function MainPage() {
     const [storeItems, setStoreItems] = useState<TItem[]>([]);
+    const [currentPage, setCurrentPage] = useState<number>(1);
 
     useEffect(() => {
         getProducts();
-    }, []);
+    }, [currentPage]);
 
     async function getProducts() {
-        const res = await GetIds(1);
+        const res = await GetIds(currentPage);
         const items = await GetItems(res);
         setStoreItems(items);
     }
 
+    // increments currentPage state
+    function incPage() {
+        if (currentPage < 10) {
+            setCurrentPage((prev) => prev + 1);
+        }
+    }
+
+    // decrements currentPage state
+    function decPage() {
+        if (currentPage > 1) {
+            setCurrentPage((prev) => prev - 1);
+        }
+    }
+
     return (
         <main>
+            <Pagination dec={decPage} inc={incPage} page={currentPage} />
             {/* List of item cards */}
-            <section>
+            <section className={styles.cardContainer}>
                 {storeItems.map((item) => (
                     <ItemCard data={item} key={item.id} />
                 ))}
             </section>
+            <Pagination dec={decPage} inc={incPage} page={currentPage} />
         </main>
     );
 }
---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,21 @@
+import styles from './Pagination.module.css';
+
+interface PaginationProps {
+    dec: () => void;
+    inc: () => void;
+    page: number;
+}
+
+export function Pagination({ dec, inc, page }: PaginationProps) {
+    return (
+        <div className={styles.container}>
+            <button onClick={dec} disabled={page <= 1}>
+                {'<'}
+            </button>
+            <p>{page}</p>
+            <button onClick={inc} disabled={page >= 10}>
+                {'>'}
+            </button>
+        </div>
+    );
+}
